c898c765a172b6c055c00223f48117de04c483ae
Jakob <mynpmail@gmx.de>
2019-12-18 17:44:04 +0800

    Commit final 18.12.2019

--- src/TCPServer.py
+++ src/TCPServer.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
  
 import socket
- 
- 
-TCP_IP = '127.0.0.1'    # Standard loopback interface address (localhost)
-TCP_PORT = 5005         # Port to listen on (non-privileged ports are > 1023)
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
- 
+
+
+TCP_IP = ""                         # Standard loopback interface address (localhost)
+TCP_PORT = 5005                     # Port to listen on (non-privileged ports are > 1023)
+BUFFER_SIZE = 1024                  # Normally 1024, but we want fast response
+
+# create and bind socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
+print("Socket opened ...")
 
-#start listening for incoming connections
-s.listen(1)
+# start listening for incoming connections
+s.listen()
+print("Server is listening ...")
  
 conn, addr = s.accept()
 print('Connection address:', addr)
-while 1:
-    #reads data from the socket in batches of 1024 bytes using method recv() until it returns an empty string
+while True:
     data = conn.recv(BUFFER_SIZE)
-    if not data: break
+    if not data:
+        break
     print("received data:", data)
     conn.send(data)  # echo
 conn.close()
