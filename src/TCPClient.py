c9f8b8a5f4c60fdcf6f6e4fc0225b1678f3a0860
clari <hamann.clarissa@outlook.de>
2019-12-18 10:33:32 +0100

    Client final 18.12

--- src/TCPClient.py
+++ src/TCPClient.py
@@ -3,16 +3,18 @@
 import socket
  
  
-TCP_IP = '127.0.0.1'    # Standard loopback interface address (localhost)
+TCP_IP = '10.38.197.195'#'127.0.0.1'  # Standard loopback interface address (localhost)
 TCP_PORT = 5005         # Port to listen on (non-privileged ports are > 1023)
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = 'Hello'
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #it calls only connect() and immediately sends data to the server.
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
+    print('startconnect')
+    s.connect((TCP_IP, TCP_PORT))
+    print('startsend')
+    s.send(MESSAGE.encode())
+    data = s.recv(BUFFER_SIZE)
+#s.close()
 
-print("received data:", data)
+print("received data:", repr(data))
