100b07fb95e6dad300bf06314dc834dd16d85b2f
clari <hamann.clarissa@outlook.de>
2019-12-18 07:32:49 +0100

    Realsense implemented

--- BallDetection.py
+++ BallDetection.py
@@ -4,12 +4,77 @@
 
 # import the necessary packages
 from collections import deque
-from imutils.video import VideoStream
+#from imutils.video import VideoStream
 import numpy as np
 import argparse
 import cv2
-import imutils
+#import imutils
 import time
+import sys
+
+import pyrealsense2 as rs
+
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+
+def grab_contours(cnts):
+    # if the length the contours tuple returned by cv2.findContours
+    # is '2' then we are using either OpenCV v2.4, v4-beta, or
+    # v4-official
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+    # if the length of the contours tuple is '3' then we are using
+    # either OpenCV v3, v4-pre, or v4-alpha
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    # otherwise OpenCV has changed their cv2.findContours return
+    # signature yet again and I have no idea WTH is going on
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+                         "otherwise OpenCV changed their cv2.findContours return "
+                         "signature yet again. Refer to OpenCV's documentation "
+                         "in that case"))
+
+    # return the actual contours array
+    return cnts
+
+def parabel(pts):
+	temp= None
+
+
+
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -20,53 +85,71 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+#HSV
+orangeLower = (10, 170, 70)
+orangeUpper = (20, 255, 255)
 pts = deque(maxlen=args["buffer"])
+
+#timestemp for parabel calc
+time = [0]
 
 # if a video path was not supplied, grab the reference
 # to the webcam
-if not args.get("video", False):
-	vs = VideoStream(src=0).start()
+#if not args.get("video", False):
+#    vs = cv2.VideoCapture(0)
 
 # otherwise, grab a reference to the video file
-else:
-	vs = cv2.VideoCapture(args["video"])
-
+#else:
+#   vs = cv2.VideoCapture(args["video"])
+pipe = rs.pipeline()
+profile = pipe.start()
 # allow the camera or video file to warm up
-time.sleep(2.0)
+
+#time.sleep(2.0)
 
 # keep looping
 while True:
 	# grab the current frame
-	frame = vs.read()
+	#ret, frame = vs.read()
+	frames = pipe.wait_for_frames()
+	depth_frame = frames.get_depth_frame()
+	color_frame = frames.get_color_frame()
+
+	depth_image = np.asanyarray(depth_frame.get_data())
+	color_image = np.asanyarray(color_frame.get_data())
+	color_image = cv2.cvtColor(color_image, cv2.COLOR_RGB2BGR)
+	
+
+
+	#time = 
 
 	# handle the frame from VideoCapture or VideoStream
-	frame = frame[1] if args.get("video", False) else frame
-
+	#print(type(frame))
 	# if we are viewing a video and we did not grab a frame,
 	# then we have reached the end of the video
-	if frame is None:
+	if color_image is None:
+		print('Frame is none')
 		break
 
+	#cv2.imshow('frame', frame)
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	color_image = resize(color_image, width=600)
+	blurred = cv2.GaussianBlur(color_image, (11, 11), 0)
 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
 	# construct a mask for the color "green", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.inRange(hsv, orangeLower, orangeUpper)
 	mask = cv2.erode(mask, None, iterations=2)
 	mask = cv2.dilate(mask, None, iterations=2)
 
 	# find contours in the mask and initialize the current
 	# (x, y) center of the ball
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
+							cv2.CHAIN_APPROX_SIMPLE)
+	cnts = grab_contours(cnts)
 	center = None
 
 	# only proceed if at least one contour was found
@@ -81,14 +164,19 @@ while True:
 
 		# only proceed if the radius meets a minimum size
 		if radius > 10:
-			# draw the circle and centroid on the frame,
+			# draw the circle and centroid on the color_image,
 			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+			cv2.circle(color_image, (int(x), int(y)), int(radius),
+						(0, 255, 255), 2)
+			cv2.circle(color_image, center, 5, (0, 0, 255), -1)
 
 	# update the points queue
 	pts.appendleft(center)
+	
+	'''parabel'''
+	#pts in world xyz
+	#parabel(pts,time)
+
 
 	# loop over the set of tracked points
 	for i in range(1, len(pts)):
@@ -100,23 +188,18 @@ while True:
 		# otherwise, compute the thickness of the line and
 		# draw the connecting lines
 		thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+		cv2.line(color_image, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
-	# show the frame to our screen
-	cv2.imshow("Frame", frame)
+	# show the color_image to our screen
+	cv2.imshow("color_image", color_image)
 	key = cv2.waitKey(1) & 0xFF
 
 	# if the 'q' key is pressed, stop the loop
 	if key == ord("q"):
 		break
 
-# if we are not using a video file, stop the camera video stream
-if not args.get("video", False):
-	vs.stop()
 
-# otherwise, release the camera
-else:
-	vs.release()
+vs.release()
 
 # close all windows
 cv2.destroyAllWindows()
