f7fea2273f72ab0853822c57917ba70c0522f0c2
IrisAnd <iris.and@web.de>
2019-12-24 10:23:04 +0800

    Catching point calculation done

--- src/catching_point_calculation.py
+++ src/catching_point_calculation.py
@@ -8,40 +8,49 @@ def get_intersection_time(params_x,params_y,params_z):
     r_out = 600
     r_in = 300
     T_mat_poly = np.array([t*t,t,1]).transpose()
-    #print(T_mat_poly)
     T_mat_lin = np.array([t,1]).transpose()
-    #print(T_mat_lin)
-    # TODO: solve this equation for t
-    #print(params_x[0])
-    #print(params_y[2])
-    #print(params_z)
-   
-    equation = (params_x[0] * t + params_x[1])**2 +(params_y[0]*t *t + params_y[1] * t + params_y[2])**2 + (params_z[0] * t + params_z[1])**2 - r_out**2
+
+    equation = (np.dot(params_x[0], t) + params_x[1])**2 +(np.dot(params_y[0],t**2) + np.dot(params_y[1], t) + params_y[2])**2 + (np.dot(params_z[0], t) + params_z[1])**2 - r_out**2
     t = solve(equation ,t)
-    t = sym.simplify(t)
-    #t = solve((params_x*T_mat_lin)^2+(params_y*T_mat_poly)^2+(params_z*T_mat_lin)^2-r_out^2, t)
 
+    #delete complex numbers
+    print(type(t[2]))
+   
+    #need to find solution to delte complex numbers, problem data type is not a real complex number
+    t = np.array([complex(item) for item in t])
+    print(t)
+    print(type(t[2]))
+    temp =[]
+    for i in t:
+        if (i.imag == 0):
+            print(i)
+            temp.append(i)
+    t = temp
+    #should be postive, not right now because of dummy variables
+    t = min(t) 
+
+    print('t: ' + str(t))
     return t
-def get_catching_point(params_x,params_y,params_z,t):
-    x = []
-    y = []
-    z = []
-    for point in t: 
-        T_mat_poly = np.array([point*point,point,1]).transpose()
-        T_mat_lin = np.array([point,1]).transpose()
 
-        x.append(params_x*T_mat_lin)
-        y.append(params_y*T_mat_poly )
-        z.append(params_z*T_mat_lin)
-    points = (x,y,z)
-    return np.array(points)
+def get_catching_point(params_x, params_y, params_z, t):
+    
+    T_mat_poly = np.array([t*t,t,1]).transpose()
+    
+    T_mat_lin = np.array([t,1]).transpose()
+    x = np.dot(params_x, T_mat_lin)
+    y = np.dot(params_y, T_mat_poly)
+    z = np.dot(params_z, T_mat_lin)
 
-def check_boundaries(points):
-    for i in range(len(point)[0]):
-        if point[0]<0 and point[2]>0:
-            return True
-        else:
-            return False
+    return (x,y,z)
+
+def check_boundaries(point):
+    #check that x < 0 and z >0
+    print('point: ' + str(point))
+    if point[0]<0 and point[2]>0:
+        print('point: ' + str(point))
+        return True
+    else:
+        return False
 
 
 def main():
@@ -50,13 +59,11 @@ def main():
     params_z = np.array([5.,7.])
 
     t = get_intersection_time(params_x,params_y,params_z)
-    print(t)
-
-    catch_points=get_catching_point(params_x,params_y,params_z,t)
-    print(np.array(catch_points).shape)  #(dim,anzahlpoits)
-    print(len((catch_points)[0]) )
-
-    #check_boundaries(catch_points)
+  
+    catch_point=get_catching_point(params_x,params_y,params_z,t)
+    
+    if check_boundaries(catch_point):
+        print('move')
 
 
 if __name__ == "__main__":
